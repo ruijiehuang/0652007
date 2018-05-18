b3977bc11e7d848efc320736654a034174b18804
ruijiehuang <qjjisamonkey@gmail.com>
2018-05-18 20:14:27 +0800

    Ver 3.00
    third commit

--- main.c
+++ main.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/* Class average program with counter-controlled repetition */
 
 int main(int argc, char *argv[]) {
-	unsigned int TheCounter;
-	int TheGrade;
-	int TheTotal;
-	int TheAverage;
-		
-	TheTotal = 0;
-	TheCounter = 1;
-
-	while (TheCounter <= 10) {
-	    printf("%s", "Enter grade: ");
-		scanf("%d", &TheGrade);
-		TheTotal = TheTotal + TheGrade;
-		TheCounter = TheCounter + 1;	
-	}
+	unsigned int TheCounter; // number of grade to be entered next
+	int TheGrade; // grade value
+	int TheTotal; // sum of grades entered by user
+	int TheAverage; // average of grades
 	
-	TheAverage = TheTotal / 10;
-	printf("class average is %d\n", TheAverage);
+	//initialization phase
+	TheTotal = 0; // initialize total
+	TheCounter = 1; // initialize loop counter
+	
+	//processing phase
+	while (TheCounter <= 10) { // loop 10 times
+	    printf("%s", "Enter grade: "); // prompt for input
+		scanf("%d", &TheGrade); // read grade from user
+		TheTotal = TheTotal + TheGrade; // add grade to total
+		TheCounter = TheCounter + 1; // increment counter
+		
+	}// end while
+	
+	//termination phase
+	TheAverage = TheTotal / 10; //integer division
+	
+	printf("Class average is %d\n", TheAverage); // display result
+	
 	system ("PAUSE");
-
+	
 	return 0;
-}
+} // end function main
